e1698df6f11586105bc36712c066d4c183ec1055
Ali Raza <119653263+aliraza108@users.noreply.github.com>
2025-02-20 01:11:28 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py (Do not commit this file!)
-OPENAI_API_KEY = "sk-your-actual-openai-key-here"
+OPENAI_API_KEY = "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"
 SHOPIFY_API_KEY = "eec9b509f0eea387bc84f2f552959c18"
 SHOPIFY_ACCESS_TOKEN = "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"
 SHOPIFY_SHOP_URL = "5tph5b-u3.myshopify.com"
